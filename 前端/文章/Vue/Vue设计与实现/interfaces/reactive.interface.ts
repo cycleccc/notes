ef3bbcf3ca89a6b3c4a25ffe93cd4de07f390730
yaolongfei <2991205548@qq.com>
2024-01-15 22:42:41 +0800

    vault backup: 2024-01-15 22:42:40

--- 前端/文章/Vue/Vue设计与实现/interfaces/reactive.interface.ts
+++
@@ -1,9 +0,0 @@
-interface Options {
-    immediate?: boolean;
-    lazy?: boolean;
-    flush?: string;
-    scheduler?: (effectFn: EffectFn) => void;
-    // 其他可能的字段...
-}
-
-type EffectFn = (() => void) & { options?: Options, deps: Set<> }
